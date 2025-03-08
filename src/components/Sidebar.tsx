037412adbddedae7ce8979e89b07255d5759f35b
Kfire08 <akashkatiyar08@gmail.com>
2025-03-09 04:43:04 +0530

    added sidebar icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,10 +9,24 @@ const Sidebar = () => {
             to="/"
             className={({ isActive }) =>
               isActive
-                ? "text-black block p-2 bg-gray-200"
-                : "text-black block p-2"
+                ? "text-black block p-2 bg-gray-200 flex items-center text-[18px]"
+                : "text-black block p-2 flex items-center text-[18px]"
             }
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 ml-1 mr-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"
+              />
+            </svg>
             Stores
           </NavLink>
         </li>
@@ -21,10 +35,24 @@ const Sidebar = () => {
             to="/skus"
             className={({ isActive }) =>
               isActive
-                ? "text-black block p-2 bg-gray-200"
-                : "text-black block p-2"
+                ? "text-black block p-2 bg-gray-200 flex items-center text-[18px]"
+                : "text-black block p-2 flex items-center text-[18px]"
             }
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 ml-1 mr-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
+              />
+            </svg>
             SKUs
           </NavLink>
         </li>
@@ -33,10 +61,24 @@ const Sidebar = () => {
             to="/planning"
             className={({ isActive }) =>
               isActive
-                ? "text-black block p-2 bg-gray-200"
-                : "text-black block p-2"
+                ? "text-black block p-2 bg-gray-200 flex items-center text-[18px]"
+                : "text-black block p-2 flex items-center text-[18px]"
             }
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 ml-1 mr-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+              />
+            </svg>
             Planning
           </NavLink>
         </li>
@@ -45,10 +87,24 @@ const Sidebar = () => {
             to="/charts"
             className={({ isActive }) =>
               isActive
-                ? "text-black block p-2 bg-gray-200"
-                : "text-black block p-2"
+                ? "text-black block p-2 bg-gray-200 flex items-center text-[18px]"
+                : "text-black block p-2 flex items-center text-[18px]"
             }
           >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 ml-1 mr-3"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"
+              />
+            </svg>
             Charts
           </NavLink>
         </li>
